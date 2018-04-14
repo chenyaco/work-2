9fa7ae9c37635e9fcc97ce98b68373f917d37441
chenyaco <35962393+chenyaco@users.noreply.github.com>
2018-04-14 18:26:13 +0800

    updata
    updata

--- js/all.js
+++ js/all.js
@@ -1,4 +1,5 @@
 $(document).ready(function () {
+    
     // 打開左側選單
     $('.mobile-link').click(function (event) {
         $('body').toggleClass('open');
@@ -9,14 +10,79 @@ $(document).ready(function () {
         $('body').removeClass('open');
     });
 
+    $('.navbar').click(function (event) {
+        $('body').removeClass('open');
+    });
+
+    $('content').click(function (event) {
+        $('body').removeClass('open');
+    });
+
+    
+
 
     $('.aside > .menu > li > a').click(function (event) {
         event.preventDefault();
-
         //我自己的ul做摺疊效果
         $(this).parent().find('ul').slideToggle();
-
     });
 
 
+    $('.aside > .menu > li > ul > li > a').click(function (event) {
+        event.preventDefault();
+        //我自己的ul做摺疊效果
+        $(this).parent().find('ul').slideToggle();
+    });
+
+
+
+
+
+    $(".flexslider").flexslider({
+        slideshowSpeed: 5000, //展示时间间隔ms
+        animationSpeed: 500, //滚动时间ms
+        touch: true //是否支持触屏滑动
+    });
+
+
+    //下拉式收闔選單
+    $('.dropdown').click(function (event) {
+        /* Act on the event */
+        event.preventDefault();
+        $('.dropdown').toggleClass('active');
+        $('.dropdown-open').slideToggle();
+    });
+
+
+    //top 滑動效果 
+    $('.top a').click(function (event) {
+        event.preventDefault();
+        $('html,body').animate({
+            scrollTop: 0
+        }, 1000);
+    });
+    
 });
+
+
+//置頂nav固定
+$(window).load(function () {
+    $(window).bind('scroll resize', function () {
+        var $this = $(this);
+        var $this_Top = $this.scrollTop();
+
+        //當高度小於100時，關閉區塊 
+        if ($this_Top < 100) {
+            $('nav').stop().animate({ top: "-65px" });
+        }
+        if ($this_Top > 100) {
+            $('nav').stop().animate({ top: "0px" });
+        }
+    }).scroll();
+});
+
+
+
+
+
+
